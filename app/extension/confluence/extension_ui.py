d2d8f3521529fab52563b089e5d372dcffed64fb
Firas Mkaouar <firas.mkaouar@techup.io>
2021-04-10 00:49:55 +0200

    smart tables

--- app/extension/confluence/extension_ui.py
+++ app/extension/confluence/extension_ui.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 
 from selenium_ui.base_page import BasePage
 from selenium_ui.conftest import print_timing
-from selenium_ui.confluence.pages.pages import Login, AllUpdates
+from selenium_ui.confluence.pages.pages import Login, AllUpdates, Editor
 from util.conf import CONFLUENCE_SETTINGS
 
 
@@ -12,6 +12,7 @@ def app_specific_action(webdriver, datasets):
     page = BasePage(webdriver)
     if datasets['custom_pages']:
         app_specific_page_id = datasets['custom_page_id']
+        edit_page = Editor(webdriver, app_specific_page_id)
 
     # To run action as specific user uncomment code bellow.
     # NOTE: If app_specific_action is running as specific user, make sure that app_specific_action is running
@@ -33,12 +34,19 @@ def app_specific_action(webdriver, datasets):
     # measure()
 
     @print_timing("selenium_app_custom_action")
-    def measure():
+    def measure():       
 
         @print_timing("selenium_app_custom_action:view_page")
         def sub_measure():
             page.go_to_url(f"{CONFLUENCE_SETTINGS.server_url}/pages/viewpage.action?pageId={app_specific_page_id}")
             page.wait_until_visible((By.ID, "title-text"))  # Wait for title field visible
-            page.wait_until_visible((By.ID, "tables-formula-container"))  # Wait for you app-specific UI element by ID selector
+            page.wait_until_present((By.ID, "terms-app-controller"))  # Wait for you app-specific UI element by ID selector
         sub_measure()
+
+        @print_timing("selenium_app_custom_action:edit_page")
+        def sub_measure():            
+            edit_page.go_to()
+            edit_page.wait_until_present((By.ID, "tables-formula-container"))  # Wait for you app-specific UI element by ID selector
+        sub_measure()
+
     measure()
